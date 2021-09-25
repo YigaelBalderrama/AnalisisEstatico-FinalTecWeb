203426f89f14683b489c03431dfcb48911dcf0e0
Yigael Balderrama <yigaelzero@gmail.com>
2021-09-25 19:27:45 -0400

    Oh right

--- Exceptions/DatabaseException.cs
+++ Exceptions/DatabaseException.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace SimpsonApp.Exceptions
 {
+    [Serializable]
     public class DatabaseException : Exception
     {
-        public DatabaseException(string message)
-            :base(message)
+        protected DatabaseException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
-                
+
+        }
+        public DatabaseException(string message)
+            : base(message)
+        {
+
         }
     }
 }
--- Exceptions/NotModelException.cs
+++ Exceptions/NotModelException.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace SimpsonApp.Exceptions
 {
+    [Serializable]
     public class NotModelException : Exception
     {
+        protected NotModelException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
         public NotModelException(string message)
             :base(message)
         {
